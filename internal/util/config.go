d7717a23aa9e7774fa4faae66efdb347d90cc3f0
Cookie4686 <cookieandcake1105@gmail.com>
2026-07-14 23:35:47 +0700

    fix

--- internal/util/config.go
+++ internal/util/config.go
@@ -1,54 +1,49 @@
 package util
 
 import (
-	"errors"
-	"io/fs"
+	"fmt"
 	"os"
+	"testing"
 
 	"github.com/caarlos0/env/v11"
 	"github.com/joho/godotenv"
 )
 
 type Config struct {
-	ENV string `env:"ENV" envDefault:"development"`
+	ENV string `env:"ENV"`
 	API struct {
-		PORT int `env:"PORT" envDefault:"8080"`
+		PORT int `env:"PORT"`
 	}
 	DB struct {
-		PGPORT     int    `env:"PGPORT"     envDefault:"5432"`
-		PGDATABASE string `env:"PGDATABASE" envDefault:"local"`
-		PGUSER     string `env:"PGUSER"     envDefault:"root"`
-		PGPASSWORD string `env:"PGPASSWORD" envDefault:""`
+		PGPORT     int    `env:"PGPORT"`
+		PGDATABASE string `env:"PGDATABASE"`
+		PGUSER     string `env:"PGUSER"`
+		PGPASSWORD string `env:"PGPASSWORD"`
 	}
-	ENCRYPTION_KEY string `env:"ENCRYPTION_KEY" envDefault:""`
+	ENCRYPTION_KEY string `env:"ENCRYPTION_KEY"`
 	MAIL           struct {
-		ADDRESS  string `env:"MAIL_ADDRESS"  envDefault:""`
-		SERVER   string `env:"MAIL_SERVER"   envDefault:""`
-		PORT     int    `env:"MAIL_PORT"     envDefault:"25"`
-		USERNAME string `env:"MAIL_USERNAME" envDefault:""`
-		PASSWORD string `env:"MAIL_PASSWORD" envDefault:""`
+		ADDRESS  string `env:"MAIL_ADDRESS"`
+		SERVER   string `env:"MAIL_SERVER"`
+		PORT     int    `env:"MAIL_PORT"`
+		USERNAME string `env:"MAIL_USERNAME"`
+		PASSWORD string `env:"MAIL_PASSWORD"`
 	}
 }
 
 func NewConfig() (*Config, error) {
 	environment, ok := os.LookupEnv("ENV")
-	var file string
-	if ok {
-		switch environment {
-		case "development":
-			file = ".env.development"
-		case "test":
-			file = ".env.test"
-		case "production":
-			file = ".env.production"
-		}
-
-		if err := godotenv.Load(file); err != nil && errors.Is(err, &fs.PathError{}) {
-			return nil, err
+	if !ok {
+		if testing.Testing() {
+			environment = "test"
+		} else {
+			environment = "production"
 		}
 	}
-	if err := godotenv.Load(".env"); err != nil && errors.Is(err, &fs.PathError{}) {
-		return nil, err
+
+	if err := godotenv.Load(fmt.Sprintf(".env.%s", environment)); err != nil {
+		if err := godotenv.Load(".env"); err != nil {
+			godotenv.Load(".env.example")
+		}
 	}
 
 	var cfg Config
